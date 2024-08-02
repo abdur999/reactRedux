b6e7e7505eed85472deceaf108f5b3fbeb90f3fb
abdur999 <abdursrahims@icloud.com>
2024-08-02 18:02:54 +0530

    baisc code setup for react project

---
+++ react_redux/src/App.js
@@ -0,0 +1,16 @@
+import Navbar from './Navbar';
+import Shop from './Shop';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <div className="container">
+      
+      <Shop/>
+    </div>
+    </>
+  );
+}
+
+export default App;
